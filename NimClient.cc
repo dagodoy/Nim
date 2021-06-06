2959778b05f1eae22d53656c3fd3918304d15968
David Godoy Ruiz <dagodoy@ucm.es>
2021-06-06 23:59:48 +0200

    Comentarios finales

--- NimClient.cc
+++ NimClient.cc
@@ -3,6 +3,7 @@
 
 int main(int argc, char **argv)
 {
+    //./nc direccion puerto nombre oponente
     NimClient ec(argv[1], argv[2], argv[3], argv[4]);
 }
 
--- NimServer.cc
+++ NimServer.cc
@@ -2,6 +2,7 @@
 
 int main(int argc, char **argv)
 {
+    //./ns direccion puerto
     P2PServer es(argv[1], argv[2]);
     es.do_messages();
 
